4e865913918e2bee38306359713bfdc371c93d67
kearaharrington <keara.harrington@gmail.com>
2022-12-06 21:39:30 -0800

    feat: add login page, function, url

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
     path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
     path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
+    path('user/<username>', views.profile, name='profile'),
+    path('login/', views.login_view, name="login"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+# Add LoginForm to this line...
+from django.contrib.auth.forms import AuthenticationForm
+# ...and add the following line...
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 # Cat model that's connected to the Database
 from .models import Cat, Dog
 
@@ -10,6 +17,12 @@ class CatCreate(CreateView):
   model = Cat
   fields = '__all__'
   success_url = '/cats'
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.user = self.request.user
+    self.object.save()
+    return HttpResponseRedirect('/cats')
 
 class CatUpdate(UpdateView):
   model = Cat
@@ -20,6 +33,7 @@ class CatUpdate(UpdateView):
     self.object.save()
     return HttpResponseRedirect('/cats/' + str(self.object.pk))
 
+@method_decorator(login_required, name='dispatch')
 class CatDelete(DeleteView):
   model = Cat
   success_url = '/cats'
@@ -72,3 +86,29 @@ def dogs_index(request):
 def dogs_show(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/show.html', { 'dog': dog })
+
+@login_required
+def profile(request, username):
+    user = User.objects.get(username=username)
+    cats = Cat.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username, 'cats': cats})
+
+def login_view(request):
+     # if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+            else:
+                print('The username and/or password is incorrect.')
+    else: # it was a get request so send the emtpy login form
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
